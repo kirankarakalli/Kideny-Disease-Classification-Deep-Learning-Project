be814907fc87455a5b7ebfdfd5e9bb3bba154744
kiran <kiran.karakalli92@gmail.com>
2025-12-12 12:49:58 +0530

    CICD using AWS

--- README.md
+++ README.md
@@ -28,4 +28,24 @@ STEP 02- install the requirements
     pip install -r requirements.txt
 
 # Finally run the following command
-python app.py
+dvc repro
+
+
+Create ECR repo to store/save docker image
+    - Save the URI: 032068930445.dkr.ecr.us-east-1.amazonaws.com/kidney
+
+
+
+sudo apt-get update -y
+
+sudo apt-get upgrade
+
+#required
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker ubuntu
+
+newgrp docker
--- src/cnnclassifier/pipeline/prediction.py
+++ src/cnnclassifier/pipeline/prediction.py
@@ -8,21 +8,17 @@ class PredictionPipeline:
         self.filename = filename
 
     def predict(self):
-        # Load model
         model = load_model(os.path.join("artifacts/training", "model.h5"))
 
-        # Load and preprocess image
+       
         img = image.load_img(self.filename, target_size=(224, 224))
         img = image.img_to_array(img)
-        img = img / 255.0      # VERY IMPORTANT
+        img = img / 255.0      
         img = np.expand_dims(img, axis=0)
 
-        # Prediction
         preds = model.predict(img)
         result = np.argmax(preds, axis=1)[0]
 
-        # result = 0 or 1
-        # YOU MUST CONFIRM THIS MAPPING FROM training.class_indices
 
         class_names = ["Normal", "Tumor"]
 
